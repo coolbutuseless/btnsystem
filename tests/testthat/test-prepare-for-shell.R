6353441e6f4d434359fc0a40d7f044017da71be7
mike <mikefc@coolbutuseless.com>
2020-04-12 22:51:53 +1000

    appveyor fixes

--- tests/testthat/test-prepare-for-shell.R
+++ tests/testthat/test-prepare-for-shell.R
@@ -1,10 +1,13 @@
 
 
 test_that("prepare_for_shell works", {
-  skip_on_appveyor()
   args <- c('hello', "hello there", "hello there.*")
+
+  expected_output    <- shQuote(args)
+  expected_output[3] <- "hello\\ there.*"
+
   expect_equal(
     prepare_for_shell(args),
-    c("'hello'", "'hello there'", "hello\\ there.*")
+    expected_output
   )
 })
